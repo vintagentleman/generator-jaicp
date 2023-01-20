3df95f3854a4e9536a72b349e57f97b3b3918e37
Konstantin Sipunin <k.sipunin@just-ai.com>
2023-01-20 21:22:36 +0600

    Clean up repo

--- generators/app/index.js
+++ generators/app/index.js
@@ -1,27 +1,25 @@
-'use strict';
-const Generator = require('yeoman-generator');
-const chalk = require('chalk');
-const yosay = require('yosay');
+"use strict";
+const Generator = require("yeoman-generator");
+const chalk = require("chalk");
+const yosay = require("yosay");
 
 module.exports = class extends Generator {
   prompting() {
     // Have Yeoman greet the user.
     this.log(
-      yosay(
-        `Welcome to the good ${chalk.red('generator-jaicp')} generator!`
-      )
+      yosay(`Welcome to the good ${chalk.red("generator-jaicp")} generator!`)
     );
 
     const prompts = [
       {
-        type: 'confirm',
-        name: 'someAnswer',
-        message: 'Would you like to enable this option?',
-        default: true
-      }
+        type: "confirm",
+        name: "someAnswer",
+        message: "Would you like to enable this option?",
+        default: true,
+      },
     ];
 
-    return this.prompt(prompts).then(props => {
+    return this.prompt(prompts).then((props) => {
       // To access props later use this.props.someAnswer;
       this.props = props;
     });
@@ -29,8 +27,8 @@ module.exports = class extends Generator {
 
   writing() {
     this.fs.copy(
-      this.templatePath('dummyfile.txt'),
-      this.destinationPath('dummyfile.txt')
+      this.templatePath("dummyfile.txt"),
+      this.destinationPath("dummyfile.txt")
     );
   }
 
